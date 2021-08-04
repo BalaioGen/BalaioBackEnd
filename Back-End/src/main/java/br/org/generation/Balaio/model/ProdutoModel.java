eba8c71d4f315d1ed4524596e0f26e3c3104128c
Matheus Tabarelli <12tabarelli@gmail.com>
2021-08-04 14:03:04 -0300

    Commit task2 - 04-08-2021

--- Back-End/src/main/java/br/org/generation/Balaio/model/ProdutoModel.java
+++ Back-End/src/main/java/br/org/generation/Balaio/model/ProdutoModel.java
@@ -1,8 +1,11 @@
 package br.org.generation.Balaio.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.util.Date;
 @Entity
@@ -11,80 +14,73 @@ public class ProdutoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotNull
     private String nomeproduto;
+    @NotNull
     private String descricaoproduto;
+    @NotNull
     private BigDecimal valor;
-    private Date datafabricacao;
-    private Date datavalidacao;
+    @NotNull
+    @JsonFormat(pattern="dd-mm-yyyy")
+    private Date dataFabricacao;
+    @JsonFormat(pattern="dd-mm-yyyy")
+    private Date dataValidade;
+    @NotNull
     private int quantidade;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNomeproduto() {
-        return nomeproduto;
-    }
-
-    public void setNomeproduto(String nomeproduto) {
-        this.nomeproduto = nomeproduto;
-    }
-
-    public String getDescricaoproduto() {
-        return descricaoproduto;
-    }
-
-    public void setDescricaoproduto(String descricaoproduto) {
-        this.descricaoproduto = descricaoproduto;
-    }
-
-    public BigDecimal getValor() {
-        return valor;
-    }
-
-    public void setValor(BigDecimal valor) {
-        this.valor = valor;
-    }
-
-    public Date getDatafabricacao() {
-        return datafabricacao;
-    }
-
-    public void setDatafabricacao(Date datafabricacao) {
-        this.datafabricacao = datafabricacao;
-    }
-
-    public Date getDatavalidacao() {
-        return datavalidacao;
-    }
-
-    public void setDatavalidacao(Date datavalidacao) {
-        this.datavalidacao = datavalidacao;
-    }
-
-    public int getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
-
+    
     @ManyToOne
-    @JsonIgnoreProperties("Produto")
+    @JsonIgnoreProperties("produto")
     private CategoriaModel categoria;
 
-    public CategoriaModel getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(CategoriaModel categoria) {
-        this.categoria = categoria;
-    }
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNomeproduto() {
+		return nomeproduto;
+	}
+	public void setNomeproduto(String nomeproduto) {
+		this.nomeproduto = nomeproduto;
+	}
+	public String getDescricaoproduto() {
+		return descricaoproduto;
+	}
+	public void setDescricaoproduto(String descricaoproduto) {
+		this.descricaoproduto = descricaoproduto;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public Date getDataFabricacao() {
+		return dataFabricacao;
+	}
+	public void setDataFabricacao(Date dataFabricacao) {
+		this.dataFabricacao = dataFabricacao;
+	}
+	public Date getDataValidade() {
+		return dataValidade;
+	}
+	public void setDataValidade(Date dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public CategoriaModel getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(CategoriaModel categoria) {
+		this.categoria = categoria;
+	}
+	
 
 /*    @OneToOne
     @JsonIgnoreProperties("Usuario")
